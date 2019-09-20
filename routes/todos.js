2db0225dc4375e441b25c8d8b886676cc41affc6
Seamus Garrity <seamusgarrity@Cleas-Air.hitronhub.home>
2019-09-20 09:17:39 -0700

    adding hhelpers/todos.js to help refractor routes/todos.js file

--- routes/todos.js
+++ routes/todos.js
@@ -2,55 +2,15 @@ var express = require('express');
 var router = express.Router();
 // var mongoose = require("mongoose");
 var db = require("../models");
+var helpers = require("../helpers/todos.js");
 
-router.get("/", function(req,res){
-    db.Todo.find()
-    .then(function(todos){
-        res.json(todos);
-    })
-    .catch(function(err){
-        res.send(err);
-    })
-})
+router.route("/")
+    .get(helpers.getTodos)
+    .post(helpers.createTodo);
 
-router.post("/", function(req,res){
-    db.Todo.create(req.body)
-    .then(function(newTodo){
-        res.status(201).json(newTodo);
-    })
-    .catch(function(err){
-        res.send(err); 
-    })
-})
-
-router.get("/:todoId", function(req, res){
-    db.Todo.findById(req.params.todoId)
-    .then(function(foundTodo){
-        res.json(foundTodo);
-    })
-    .catch(function(err){
-        res.send(err);
-    })
-})
-
-router.put("/:todoId", function(req, res){
-    db.Todo.findOneAndUpdate({_id: req.params.todoId}, req.body, {new:true})
-    .then(function(newTodo){
-        res.json(newTodo);
-    })
-    .catch(function(err){
-        res.send(err);
-    })
-});
-
-router.delete("/:todoId", function(req, res){
-    db.Todo.findOneAndDelete({_id: req.params.todoId})
-    .then(function(){
-        res.json({message: "we deleted it"});
-    })
-    .catch(function(err){
-        res.send(err);
-    })
-});
+router.route("/:todoId")
+    .get(helpers.findTodo)
+    .put(helpers.updateTodo)
+    .delete(helpers.deleteTodo)
 
 module.exports = router;
